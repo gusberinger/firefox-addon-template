c3faa1099f9a1d4bc0b4ac124e7a5351c13bbbf3
Gus Beringer <gusberinger@gmail.com>
2022-12-10 15:38:58 -0600

    enforce strict typing rules

--- popup/popup-script.ts
+++ popup/popup-script.ts
@@ -4,34 +4,15 @@ const textNode = document.getElementById("text")
 const amountNode = document.getElementById("amount")
 const resultNode = document.getElementById("result")
 const withNode = document.getElementById("with")
-const leftpadForm = document.getElementById("leftpad-form")
+const bgPad = document.getElementById("pad-bg")
 
-if (
-	!(
-		textNode instanceof HTMLInputElement &&
-		amountNode instanceof HTMLInputElement &&
-		withNode instanceof HTMLInputElement &&
-		resultNode instanceof HTMLOutputElement &&
-		leftpadForm instanceof HTMLFormElement
-	)
-)
-	throw new Error("Element was not correct type")
+if (!(textNode instanceof HTMLInputElement)) throw new Error("Not input")
+if (!(amountNode instanceof HTMLInputElement)) throw new Error("Not input")
+if (!(resultNode instanceof HTMLInputElement)) throw new Error("Not input")
+if (!(withNode instanceof HTMLInputElement)) throw new Error("Not input")
+if (!(bgPad instanceof HTMLButtonElement)) throw new Error("Not button")
 
-if (!amountNode)
-	leftpadForm.addEventListener(
-		"submit",
-		(e) => {
-			e.preventDefault()
-			resultNode.value = leftPad(
-				textNode.value,
-				amountNode.valueAsNumber,
-				withNode.value
-			)
-		},
-		false
-	)
-
-document.getElementById("pad-bg").addEventListener("click", (e) => {
+bgPad.addEventListener("click", (e) => {
 	let sendingMessage = browser.runtime.sendMessage({
 		text: textNode.value,
 		amount: amountNode.valueAsNumber,
