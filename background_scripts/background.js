b6ca8bf8031232fcad861e35a664a57cecc30ca7
Gus Beringer <gusberinger@gmail.com>
2022-12-10 13:14:36 -0600

    installed prettier

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -1,6 +1,6 @@
-const leftPad = require("left-pad");
+const leftPad = require("left-pad")
 
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const result = leftPad(message.text, message.amount, message.with);
-    sendResponse(result);
-});
+	const result = leftPad(message.text, message.amount, message.with)
+	sendResponse(result)
+})
