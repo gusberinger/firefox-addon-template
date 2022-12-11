cac337295ef14fb07fcff6e25bf011cb4c8d46df
Gus Beringer <gusberinger@gmail.com>
2022-12-11 14:39:27 -0600

    switched to using dist folder for webpack builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,53 @@
 const path = require("path")
 const TerserPlugin = require("terser-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const TransformJson = require("transform-json-webpack-plugin")
+const package = require("./package.json")
 
 const PROD = JSON.parse(process.env.NODE_ENV == "production")
 
+const _resolve = {
+	extensions: [".ts"],
+	modules: [path.resolve(__dirname, "node_modules"), "node_modules"],
+}
+
+const _module = {
+	rules: [
+		{
+			test: /\.tsx?$/,
+			use: "ts-loader",
+			exclude: /node_modules/,
+		},
+	],
+}
+
 module.exports = {
-	entry: {
-		background_scripts: "./background_scripts/background.ts",
-		popup: "./popup/popup-script.ts",
-	},
+	entry: [path.resolve(__dirname, "src", "background", "background.ts")],
 	output: {
-		path: path.resolve(__dirname, "addon"),
-		filename: "[name]/index.js",
+		path: path.resolve(__dirname, "dist"),
+		filename: path.join("background", "background.js"),
 	},
-	resolve: {
-		extensions: [".ts"],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/,
+	resolve: _resolve,
+	module: _module,
+	plugins: [
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, "static", "**", "*"),
+					to: "./",
+					context: "static/",
+				},
+			],
+		}),
+		new TransformJson({
+			source: path.resolve(__dirname, "src", "manifest.json"),
+			filename: "manifest.json",
+			object: {
+				description: package.description,
+				version: package.version,
 			},
-		],
-	},
-	mode: "none",
+		}),
+	],
 	optimization: {
 		minimize: PROD,
 		minimizer: [new TerserPlugin({ parallel: true })],
